829aaf0c5947a4aee75b8ae9a60bdb97bf4758a9
phorton1 <available@upon.request>
2022-12-28 10:21:00 -0500

    simplify testDevice by removing OTHER_LED

--- examples/testDevice/testDevice.ino
+++ examples/testDevice/testDevice.ino
@@ -5,14 +5,6 @@
 #include <myIOTDevice.h>
 #include <myIOTLog.h>
 
-#if WITH_SD
-    #define INIT_SD_EARLY  1
-    #if INIT_SD_EARLY
-        #include <SD.h>
-    #endif
-#endif
-
-
 //------------------------
 // testDevice definition
 //------------------------
@@ -21,10 +13,8 @@
 #define TEST_DEVICE_VERSION     "t1.0"
 
 #define PIN_ONBOARD_LED     2
-#define PIN_OTHER_LED       33      // for old test ui - not on actual bilgeAlarm board
 
 #define ID_ONBOARD_LED      "ONBOARD_LED"
-#define ID_OTHER_LED        "OTHER_LED"
 #define ID_DEMO_MODE        "DEMO_MODE"
 
 
@@ -32,15 +22,14 @@
 
 static valueIdType dash_items[] = {
     ID_ONBOARD_LED,
-    ID_OTHER_LED,
     ID_DEMO_MODE,
     ID_REBOOT,
     0
 };
 
-// what shows up on the "device" UI tab
+// what shows up on the "config" UI tab
 
-static valueIdType device_items[] = {
+static valueIdType config_items[] = {
     ID_DEMO_MODE,
     0
 };
@@ -57,7 +46,6 @@ public:
     {
         LOGD("testDevice::setup(%s)",getVersion());
         pinMode(PIN_ONBOARD_LED,OUTPUT);
-        pinMode(PIN_OTHER_LED,OUTPUT);
         myIOTDevice::setup();
     }
 
@@ -68,14 +56,13 @@ private:
     static const valDescriptor m_test_values[];
 
     static bool _ONBOARD_LED;
-    static bool _OTHER_LED;
     static bool _DEMO_MODE;
 
     static void onLed(const myIOTValue *desc, bool val)
     {
         String id = desc->getId();
         LOGD("onLed(%s,%d)",id.c_str(),val);
-        digitalWrite(id == ID_ONBOARD_LED?PIN_ONBOARD_LED:PIN_OTHER_LED,val);
+        digitalWrite(PIN_ONBOARD_LED,val);
     }
 
 };
@@ -88,7 +75,6 @@ const valDescriptor testDevice::m_test_values[] =
     { ID_DEVICE_NAME,      VALUE_TYPE_STRING,   VALUE_STORE_PREF,     VALUE_STYLE_REQUIRED,   NULL,   NULL,   TEST_DEVICE },
         // override base class element
     { ID_ONBOARD_LED,      VALUE_TYPE_BOOL,     VALUE_STORE_TOPIC,    VALUE_STYLE_NONE,       (void *) &_ONBOARD_LED,    (void *) onLed, },
-    { ID_OTHER_LED,        VALUE_TYPE_BOOL,     VALUE_STORE_TOPIC,    VALUE_STYLE_NONE,       (void *) &_OTHER_LED,      (void *) onLed, },
     { ID_DEMO_MODE,        VALUE_TYPE_BOOL,     VALUE_STORE_PREF,     VALUE_STYLE_NONE,       (void *) &_DEMO_MODE,       NULL,          },
 };
 
@@ -99,13 +85,12 @@ const valDescriptor testDevice::m_test_values[] =
 testDevice::testDevice()
 {
     addValues(m_test_values,NUM_DEVICE_VALUES);
-    setTabLayouts(dash_items,device_items);
+    setTabLayouts(dash_items,config_items);
 }
 
 // static member data
 
 bool testDevice::_ONBOARD_LED;
-bool testDevice::_OTHER_LED;
 bool testDevice::_DEMO_MODE;
 
 
@@ -124,17 +109,6 @@ void setup()
 
     testDevice::setDeviceType(TEST_DEVICE);
     testDevice::setDeviceVersion(TEST_DEVICE_VERSION);
-
-
-    // init the SD Card in early derived device
-    // due to it's wonky SPI behavior, and so that
-    // logfile can begin immediately.
-
-    #if WITH_SD
-    #if INIT_SD_EARLY
-        bool sd_ok = testDevice::initSDCard();
-    #endif
-    #endif
 
     LOGU("");
     LOGU("");
@@ -158,17 +132,15 @@ void loop()
 {
     test_device->loop();
 
-    #if 1
-        if (test_device->getBool(ID_DEMO_MODE))
+    if (test_device->getBool(ID_DEMO_MODE))
+    {
+        uint32_t now = millis();
+        static uint32_t toggle_led = 0;
+        if (now > toggle_led + 2000)
         {
-            uint32_t now = millis();
-            static uint32_t toggle_led = 0;
-            if (now > toggle_led + 2000)
-            {
-                toggle_led = now;
-                bool led_state = test_device->getBool(ID_ONBOARD_LED);
-                test_device->setBool(ID_ONBOARD_LED,!led_state);
-            }
+            toggle_led = now;
+            bool led_state = test_device->getBool(ID_ONBOARD_LED);
+            test_device->setBool(ID_ONBOARD_LED,!led_state);
         }
-    #endif
+    }
 }
